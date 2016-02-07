81e1eab79ab84cc885de1190e6cb8b579d20a957
Swati Maheshwari <er.swatimaheshwari@gmail.com>
2016-02-07 16:09:13 +0530

    script.js
    call test function to sort array

---
+++ Script.js
@@ -0,0 +1,23 @@
+// Code goes here
+
+var test= function()
+{
+  var price =[100,500,200,-100,300];
+  this.sort(price);
+}
+
+var sort= function(price)
+{
+  var sortedArray= [];
+  var leastNum = price[0];
+  var priceCopy = price.slice();
+  
+ for (i = 0; i < price.length; i++)
+  {
+   var minNum=  Math.min.apply( Math, priceCopy );
+    sortedArray.push(minNum);  
+    priceCopy.splice(priceCopy.indexOf(minNum),1);
+  }
+  
+  return sortedArray;
+}
